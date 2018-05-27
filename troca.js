3fe6cbdeecf63ffd0627049e070f417835974206
Catarina Lucas <catarinalucasloureiro@gmail.com>
2018-05-27 22:09:02 +0100

    round2
    landing acabada, numeros tambem

---
+++ troca.js
@@ -0,0 +1,31 @@
+function replaceUm(){
+    document.getElementById("numeros__interacao").innerHTML = "Crimes Céleres";
+}
+
+function replaceDois(){
+    document.getElementById("numeros__interacao").innerHTML = "Impressões Genitais";
+}
+
+function replaceTres(){
+    document.getElementById("numeros__interacao").innerHTML = "Retratos de Um Delirium Album";
+}
+
+function replaceQuatro(){
+    document.getElementById("numeros__interacao").innerHTML = "Story a Bordo de Um Crime";
+}
+
+function replaceCinco(){
+    document.getElementById("numeros__interacao").innerHTML = "Fotos de Um Homem Morto";
+}
+
+function replaceSeis(){
+    document.getElementById("numeros__interacao").innerHTML = "Álbum de Família de Afonso Colite";
+}
+
+function replaceSete(){
+    document.getElementById("numeros__interacao").innerHTML = "Instantâneos";
+}
+
+function replaceLess(){
+    document.getElementById("numeros__interacao").innerHTML = "";
+}
